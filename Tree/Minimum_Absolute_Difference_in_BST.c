870e61236072a80117c7e8f4ec614f7c80d4f43b
Resister <zhuyyyyyyyy@gmail.com>
2019-02-13 14:50:04 +0800

    Update Minimum_Absolute_Difference_in_BST.c

--- Tree/Minimum_Absolute_Difference_in_BST.c
+++ Tree/Minimum_Absolute_Difference_in_BST.c
@@ -6,34 +6,55 @@
 //  Copyright © 2019 zyy. All rights reserved.
 //
 
-static int compare(const void *a, const void *b)
-{
-    return *(int *) a - *(int *) b;
+int minfour(int a, int b, int c, int d) {
+    int min = INT_MAX;
+    if(min > a && a > 0){
+        min = a;
+    }
+    if(min > b && b > 0) {
+        min = b;
+    }
+    if(min > c && c > 0) {
+        min = c;
+    }
+    if(min > d && d > 0) {
+        min = d;
+    }
+    return min;
 }
 
-int
-findPairs(int *nums, int numsSize, int k)
-{
-    int pair = 0;
-    int i, j;
+int helper(struct TreeNode *root, int *min, int *max) {
     
-    qsort(nums, numsSize, sizeof(int), compare);
-    //快速排序
-    i = j = 0;  //因为排序了
-    while (i < numsSize && j < numsSize) {
-        if (i == j || nums[i] + k > nums[j]) {
-            j++;  //升序排序后，向前加，i数加k找前j，
-        } else if (nums[i] + k < nums[j]) {
-            i++;
-        } else {
-            pair++; //如果吻合的话，下一个和必然更大，小小互减，大大互减
-            i++;
-            while (j < numsSize - 1 && nums[j] == nums[j + 1]) {
-                j++;  //假如重复再跳一位
-            }
-            j++;
-        }
+    if(!root) {
+        *min = INT_MAX;
+        *max = INT_MIN;
+        return INT_MAX;
     }
     
-    return pair;
+    if (!(root->left) && !(root->right)) {
+        *min = root->val;
+        *max = root->val;
+        return INT_MAX;
+    }
+    
+    int min_l, max_l;
+    int min_r, max_r;
+    
+    int l_res = helper(root->left, &min_l, &max_l);
+    int r_res = helper(root->right, &min_r, &max_r);
+    //所有里面的最大最小值，所有的最短距离
+    *min = (min_l < root->val ? min_l : root->val);
+    *max = (max_r > root->val ? max_r : root->val);
+    
+    //Note : abs(max_l - root->val) or abs(min_r - root->val) will over flow if there was no left or right child
+    //So check that the result is > than 0 in minfour function
+    return minfour(abs(max_l - root->val), abs(min_r - root->val), l_res, r_res);
+    
+}
+
+int getMinimumDifference(struct TreeNode* root) {
+    
+    int min = INT_MAX;
+    int max = INT_MIN;
+    return helper(root, &min, &max);
 }
